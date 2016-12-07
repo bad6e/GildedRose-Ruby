6bc02c4186423c805c1ca4c3f86ca65d96a42726
Bret Doucette <doucette.bret@gmail.com>
2016-12-07 14:04:57 -0700

    Broke test and lib out - added item test

---
+++ ruby/lib/gilded_rose.rb
@@ -0,0 +1,71 @@
+class GildedRose
+
+  def initialize(items)
+    @items = items
+  end
+
+  def update_quality()
+    @items.each do |item|
+
+
+
+     #What does this block do - this decreases quality for generics items - only if quality is > 0
+      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+
+        if item.quality > 0
+
+          if item.name != "Sulfuras, Hand of Ragnaros"
+            item.quality = item.quality - 1
+          end
+
+        end
+
+      else
+
+
+
+        if item.quality < 50
+          item.quality = item.quality + 1
+          if item.name == "Backstage passes to a TAFKAL80ETC concert"
+            if item.sell_in < 11
+              if item.quality < 50
+                item.quality = item.quality + 1
+              end
+            end
+
+            if item.sell_in < 6
+
+              if item.quality < 50
+                item.quality = item.quality + 1
+              end
+
+            end
+
+          end
+        end
+      end
+
+
+      if item.name != "Sulfuras, Hand of Ragnaros"
+        item.sell_in = item.sell_in - 1
+      end
+      if item.sell_in < 0
+        if item.name != "Aged Brie"
+          if item.name != "Backstage passes to a TAFKAL80ETC concert"
+            if item.quality > 0
+              if item.name != "Sulfuras, Hand of Ragnaros"
+                item.quality = item.quality - 1
+              end
+            end
+          else
+            item.quality = item.quality - item.quality
+          end
+        else
+          if item.quality < 50
+            item.quality = item.quality + 1
+          end
+        end
+      end
+    end
+  end
+end
---
+++ ruby/test/item_test.rb
@@ -0,0 +1,29 @@
+require_relative "test_helper"
+require "./lib/item"
+
+class ItemTest < Minitest::Test
+
+  def setup
+    @item = Item.new("test_name", 2, 3)
+  end
+
+  def test_it_creates_an_item
+    assert(@item)
+  end
+
+  def test_it_has_a_name_attribute
+    assert_equal("test_name", @item.name)
+  end
+
+  def test_it_has_a_sell_in_attribute
+    assert_equal(2, @item.sell_in)
+  end
+
+  def test_it_has_a_quality_attribute
+    assert_equal(3, @item.quality)
+  end
+
+  def test_it_concatenates_3_attributes
+    assert_equal("test_name, 2, 3", @item.to_s())
+  end
+end
