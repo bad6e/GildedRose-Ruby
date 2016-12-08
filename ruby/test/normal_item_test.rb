13a6f96c19b8144c848b81ba3ebcb93ecb8b9eee
Bret Doucette <doucette.bret@gmail.com>
2016-12-08 10:15:41 -0700

    Refactor normal item and conjured

--- ruby/test/normal_item_test.rb
+++ ruby/test/normal_item_test.rb
@@ -1,7 +1,7 @@
 require_relative "test_helper"
 
 class NormalItemTest < Minitest::Test
-  def test_wolf_tshirt_sell_in_value_decreases_by_1_at_the_end_of_each_day
+  def test_wolf_tshirt_sell_in_decreases_by_1_at_the_end_of_each_day
     item = Item.new("Wolf T-Shirt", 2, 5)
     NormalItem.new(item)
     assert_equal(1, item.sell_in)
@@ -37,9 +37,23 @@ class NormalItemTest < Minitest::Test
     assert_equal(5, item.quality)
   end
 
-  def test_when_wolf_tshirt_sell_in_date_is_than_0_qal_decreases_twice_as_fast_but_never_negative
+  def test_when_wolf_tshirt_sell_in_date_is_less_than_0_qal_decreases_twice_as_fast_but_never_negative
     item = Item.new("Wolf T-Shirt", -1, 0.5)
     NormalItem.new(item)
     assert_equal(0, item.quality)
+  end
+
+  def test_text_fixture_data_1
+    item = Item.new("+5 Dexterity Vest", 10, 20)
+    NormalItem.new(item)
+    assert_equal(9, item.sell_in)
+    assert_equal(19, item.quality)
+  end
+
+  def test_text_fixture_data_2
+    item = Item.new("Elixir of the Mongoose", 5, 7)
+    NormalItem.new(item)
+    assert_equal(4, item.sell_in)
+    assert_equal(6, item.quality)
   end
 end
